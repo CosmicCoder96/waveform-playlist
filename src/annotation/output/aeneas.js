addbeced1e78a2978653a4b91ebd3d7bbf524f14
Abhinav Khare <abhinav.khare31@gmail.com>
2019-03-26 11:20:28 +0800

    Update aeneas.js

--- src/annotation/output/aeneas.js
+++ src/annotation/output/aeneas.js
@@ -1,21 +1,16 @@
-/*
-{
-  "begin": "5.759",
-  "end": "9.155",
-  "id": "002",
-  "language": "en",
-  "lines": [
-    "I just wanted to hold"
-  ]
-},
- */
+"use strict";
 
-export default function (annotation) {
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+exports.default = function (annotation) {
   return {
     begin: String(annotation.start.toFixed(3)),
     end: String(annotation.end.toFixed(3)),
     id: String(annotation.id),
     language: annotation.lang,
     lines: annotation.lines,
+    speaker: annotation.speaker
   };
-}
+};
